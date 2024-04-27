5a6661c339cdff6f262e9c662c2a099804600500
Fátima <146662700+fatiprogramming@users.noreply.github.com>
2024-04-27 20:27:19 -0300

    Add files via upload

---
+++ primerprograma/ArraysPrueba.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+public class ArraysPrueba {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1 = new ArrayList<>(Arrays.asList("Apple", "Banana", "Orange"));
+        System.out.println(arrayList1.get(1));
+    }
+}
